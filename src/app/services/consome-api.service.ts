be4518a9109774b5b1d1bb5ee42cfd19930aac8c
ElLagueadox <tahir.rodriguez.l@gmail.com>
2023-09-29 17:25:03 -0300

    a

--- src/app/services/consome-api.service.ts
+++ src/app/services/consome-api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { retry, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsomeAPIService {
+  httpOptions = {
+    headers: new HttpHeaders({
+    'Content-Type':'application/json',
+    'Access-Control-Allow-Origin' :'*'
+  })
+  }
 
-  constructor() { }
+  apiURL = 'https://jsonplaceholder.typicode.com'
+
+  constructor(private http:HttpClient) { }
 }
