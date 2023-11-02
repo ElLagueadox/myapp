326cd03339014489f9809b917412578e67fb8f99
ElLagueadox <tahir.rodriguez.l@gmail.com>
2023-11-02 15:09:36 -0300

    a

--- src/app/pages/home2/home2.page.ts
+++ src/app/pages/home2/home2.page.ts
@@ -4,8 +4,8 @@ import { ConsomeAPIService } from 'src/app/services/consome-api.service';
 
 @Component({
   selector: 'app-home2',
-  templateUrl: './home2.page.html',
-  styleUrls: ['./home2.page.scss'],
+  templateUrl: 'home2.page.html',
+  styleUrls: ['home2.page.scss'],
 })
 export class Home2Page implements OnInit {
 
